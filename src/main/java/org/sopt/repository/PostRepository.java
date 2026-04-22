42252f4165d177ea25e8eeeb813489301af61a0c
jstar_00 <wltjd6300@naver.com>
2026-04-22 20:19:58 +0900

    feat: @Repository 어노테이션 적용

--- src/main/java/org/sopt/repository/PostRepository.java
+++ src/main/java/org/sopt/repository/PostRepository.java
@@ -2,9 +2,15 @@ package org.sopt.repository;
 
 import org.sopt.domain.Post;
 
+import org.springframework.stereotype.Repository;
+
 import java.util.ArrayList;
 import java.util.List;
 
+// @Repository -> Spring이 이 클래스를 Bean으로 등록하고 관리함
+// 앱 시작 시 Spring이 PostRepository 인스턴스를 만들어 Container에 보관
+// 이후 PostService 등이 이 인스턴스를 주입받아 사용
+@Repository
 public class PostRepository {
     private final List<Post> postList = new ArrayList<>();
     private Long nextId = 1L;
