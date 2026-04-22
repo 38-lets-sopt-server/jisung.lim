156a751900a47accb475d40b8046efa3b5d88583
jstar_00 <wltjd6300@naver.com>
2026-04-22 20:24:55 +0900

    feat: PostValidator에 @Component 어노테이션 적용

--- src/main/java/org/sopt/service/PostValidator.java
+++ src/main/java/org/sopt/service/PostValidator.java
@@ -2,7 +2,14 @@ package org.sopt.service;
 
 import org.sopt.domain.Post;
 import org.sopt.exception.PostNotFoundException;
+import org.springframework.stereotype.Component;
 
+// @Repository, @Service, @Controller 등의 어노테이션이 있음
+// Validator는 저장소/Service/Controller 모두 아님 -> 범용 Bean으로
+// 등록할 때 쓰는 @Component가 적절, 기능은 다른 어노테이션과 동일(Spring Bean으로 등록, 필요한 곳에 주입)
+// PostService가 new PostValidator()를 버리고 생성자 주입으로 PostValidator를 받으려면
+// PostValidator도 Spring이 관리하는 Bean이어야 함
+@Component
 public class PostValidator {
 
     private static final int MAX_TITLE_LENGTH = 50;
