e97310da836f4b50d1102b9195e93d032490d649
jstar_00 <wltjd6300@naver.com>
2026-05-01 15:08:58 +0900

    feat: User, UserRepository 생성
    - User 엔티티는 DB와 연동
    - UserRepository는 JpaRepository 상속 -- 기본 CRUD 메서드 사용 가능

---
+++ src/main/java/org/sopt/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package org.sopt.repository;
+
+import org.sopt.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+// 인터페이스가 JpaRepository를 상속하면 Spring Data JPA가
+// 자동으로 Bean으로 등록해주므로 @Repository는 필수 X, 관례상 붙이는 것도 허용
+// extends JpaRepository: save, findById, count, delete, 페이지네이션 등
+//   기본 CRUD 메서드 + a 다양하게 제공
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
